69d57ecf2f6d7c958c8aa542d55f7c017e8e4961
Chris Schnabel <chrisjschnabel@gmail.com>
2020-04-16 18:48:49 -0400

    Solution for 14

---
+++ 14_longest_common_prefix.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+def longest_common_prefix(strs: List[str]) -> str:
+    if strs is None or len(strs) == 0:
+        return ''
+    elif len(strs) == 1:
+        return strs[0]
+
+    prefix = ''
+    i = 0
+
+    while i is not None:
+        same_ch = True
+        if len(strs[0]) == 0 or i >= len(strs[0]):
+            break
+        ch = strs[0][i]
+        
+        for s in strs[1:]:
+            if i < len(s) and s[i] == ch:
+                same_ch = True
+            else:
+                same_ch = False
+                i = None
+                break
+
+        if same_ch:
+            prefix += ch
+            i += 1
+
+    print(prefix)
+    return prefix
+
+
+
+# Expect: 'fl'
+longest_common_prefix(['flower', 'flow', 'flight'])
+# Expect: ''
+longest_common_prefix(['dog', 'racecar', 'car'])
+# Expect: 'c'
+longest_common_prefix(['c', 'c'])
