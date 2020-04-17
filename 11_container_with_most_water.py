88cc37e354fd2866cb3971781b6ac091b59095a2
Chris Schnabel <chrisjschnabel@gmail.com>
2020-04-16 20:05:14 -0400

    Solution for 11

---
+++ 11_container_with_most_water.py
@@ -0,0 +1,40 @@
+from typing import List
+
+# O(n^2) -> TOO SLOW
+def max_area_bad(height: List[int]) -> int:
+    l = len(height)
+    max = 0
+
+    for i in range(0, l):
+        for j in range(i + 1, l):
+            area = (j - i) * min(height[i], height[j])
+            if area > max:
+                max = area
+
+    print(max)
+    return max
+
+def max_area(height: List[int]) -> int:
+    max = 0
+    i = 0
+    j = len(height) - 1
+
+    while i < j:
+        hi = height[i]
+        hj = height[j]
+        if hi > hj:
+            area = (j - i) * hj
+            j -= 1
+        else:
+            area = (j - i) * hi
+            i += 1
+        
+        if area > max:
+            max = area
+
+    print(max)
+    return max
+
+
+# Expect: 49
+max_area([1,8,6,2,5,4,8,3,7])
