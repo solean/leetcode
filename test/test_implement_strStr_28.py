600167421096f11f463a1754b88368fa501dcf6c
Chris Schnabel <chrisjschnabel@gmail.com>
2020-04-22 14:50:30 -0400

    Removed accidental tabs

--- test/test_implement_strStr_28.py
+++ test/test_implement_strStr_28.py
@@ -5,10 +5,10 @@ class TestImplementStrStr(unittest.TestCase):
 
     def test_empty_string(self):
         self.assertEqual(strStr('test', ''), 0)
-    
+
     def test_not_found(self):
         self.assertEqual(strStr('aaaaa', 'bba'), -1)
-    
+
     def test_found(self):
         self.assertEqual(strStr('hello', 'll'), 2)
 
