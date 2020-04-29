2a821c58f54a8f51de019f3b30d8044c5b4bd46a
Chris Schnabel <chrisjschnabel@gmail.com>
2020-04-28 22:07:13 -0400

    Faster O(log n) solution for 35, using binary search.

--- search_insert_position_35.py
+++ search_insert_position_35.py
@@ -1,8 +1,26 @@
 from typing import List
+import math
 
-def search_insert(nums: List[int], target: int) -> int:
+# O(n) solution
+def search_insert_o_n(nums: List[int], target: int) -> int:
     for i in range(0, len(nums)):
         if nums[i] >= target:
             return i
     return len(nums)
+
+# O(log n) solution (binary search)
+def search_insert(nums: List[int], target: int) -> int:
+    l = 0
+    h = len(nums) - 1
+
+    while l <= h:
+        mid = math.floor((l + h ) / 2)
+        if nums[mid] < target:
+            l = mid + 1
+        elif nums[mid] > target:
+            h = mid - 1
+        else:
+            return mid
+
+    return l
 
