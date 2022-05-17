6b2f53360684ed11008f47dee9fd202cd7481d0a
Chris Schnabel <chrisjschnabel@gmail.com>
2022-05-16 23:03:53 -0400

    Another solution for 347

--- top_k_frequent_elements_347.py
+++ top_k_frequent_elements_347.py
@@ -16,3 +16,25 @@ def top_k_frequent(nums: List[int], k: int) -> List[int]:
         res.append(freq[i][0])
 
     return res
+
+def another_solution(nums: List[int], k: int) -> List[int]:
+    d = {}
+
+    for n in nums:
+        if n in d:
+            d[n] += 1
+        else:
+            d[n] = 1
+
+    bucket = [[] for i in range(len(nums) + 1)]
+
+    for key, count in d.items():
+        bucket[count].append(key)
+
+    for i in range(len(bucket) - 1, 0, -1):
+        if bucket[i]:
+            freq += bucket[i]
+
+            if len(freq) >= k:
+                return freq[:k]
+
