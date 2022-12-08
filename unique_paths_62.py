d5987e37739a6b959046c54f7d518006c88cb38f
Chris Schnabel <chrisjschnabel@gmail.com>
2022-12-08 15:30:05 -0500

    Simpler solution to 62 - Unique Paths

--- unique_paths_62.py
+++ unique_paths_62.py
@@ -9,21 +9,15 @@ def unique_paths_recursive(m: int, n: int) -> int:
     return right + down
 
 
-# Faster math solution
+# Faster dp solution
 def unique_paths(m: int, n: int) -> int:
     if m == 1 or n == 1:
         return 1
 
-    counts = [[0 for x in range(m)] for y in range(n)]
+    dp = [[1 for _ in range(m)] for _ in range(n)]
 
-    for i in range(n - 1, -1, -1):
-        for j in range(m - 1, -1, -1):
-            if i == n-1 and j == m-1:
-                counts[i][j] = 0
-            elif i == n-1 or j == m-1:
-                counts[i][j] = 1
-            else:
-                counts[i][j] = counts[i+1][j] + counts[i][j+1]
+    for r in range(m - 2, -1, -1):
+        for c in range(n - 2, -1, -1):
+            dp[r][c] = dp[r][c + 1] + dp[r + 1][c]
 
-    return counts[0][0]
-
+    return dp[0][0]
