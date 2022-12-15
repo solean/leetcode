d96777d8d91123547df1b2d3682388a30f6d3f86
Chris Schnabel <chrisjschnabel@gmail.com>
2022-12-14 21:57:10 -0500

    Solution to 300 - Longest Increasing Subsequence

---
+++ longest_increasing_subsequence_300.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def length_of_lis(nums: List[int]) -> int:
+    dp = [1] * len(nums)
+
+    for i in range(len(nums) - 1, -1, -1):
+        for j in range(i + 1, len(nums)):
+            if nums[i] < nums[j]:
+                dp[i] = max(dp[i], 1 + dp[j])
+
+    return max(dp)
