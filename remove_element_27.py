1bf62b83e8803828693a4ce203edeeef87790d85
Chris Schnabel <chrisjschnabel@gmail.com>
2024-01-12 19:39:43 -0500

    New solution to 27 - Remove Element

--- remove_element_27.py
+++ remove_element_27.py
@@ -10,3 +10,17 @@ def remove_element(nums: List[int], val: int) -> int:
 
     return len(nums)
 
+# New question requires elements to be "removed" in place (moved to end of array)
+def remove_element_new(nums: List[int], val: int) -> int:
+    i = 0
+    j = len(nums)
+
+    while i < j:
+        if nums[i] == val:
+            nums[i], nums[j - 1], nums[j - 1], nums[i]
+            j -= 1
+        else:
+            i += 1
+
+    return j
+
