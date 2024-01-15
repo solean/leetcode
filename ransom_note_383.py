d7450ff5169a94f07254010383ed6211615376f1
Chris Schnabel <chrisjschnabel@gmail.com>
2024-01-14 22:26:42 -0500

    Better solution to 383 - Ransom Note

--- ransom_note_383.py
+++ ransom_note_383.py
@@ -1,23 +1,15 @@
+from collections import defaultdict
 
 def can_construct(ransom_note: str, magazine: str) -> bool:
-    mag_char_counts = dict()
-    for ch in magazine:
-        if ch in mag_char_counts:
-            mag_char_counts[ch] += 1
-        else:
-            mag_char_counts[ch] = 1
+    chars = defaultdict(int)
 
-    ransom_char_counts = dict()
-    for ch in ransom_note:
-        if ch not in mag_char_counts:
+    for mch in magazine:
+        chars[ch] += 1
+
+    for rch in ransom_note:
+        if chars[rch] == 0:
             return False
-
-        if ch in ransom_char_counts:
-            ransom_char_counts[ch] += 1
-            if ransom_char_counts[ch] > mag_char_counts[ch]:
-                return False
-        else:
-            ransom_char_counts[ch] = 1
+        chars[rch] -= 1
 
     return True
 
