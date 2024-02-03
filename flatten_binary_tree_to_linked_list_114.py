004220bfb1dcb31344bbba1ed0765ca5045a6781
Chris Schnabel <chrisjschnabel@gmail.com>
2024-02-02 20:35:12 -0500

    Better solution to 114 that doesn't use extra space (other than recursion stack)

--- flatten_binary_tree_to_linked_list_114.py
+++ flatten_binary_tree_to_linked_list_114.py
@@ -17,3 +17,23 @@ def flatten(root: TreeNode) -> None:
         node = traversal[i]
         node.left = None
         node.right = traversal[i + 1]
+
+
+def flatten_no_extra_space(root: TreeNode) -> None:
+
+    def dfs(node):
+        if not node:
+            return None
+        
+        left_tail = dfs(node.left)
+        right_tail = dfs(node.right)
+
+        if left_tail:
+            left_tail.right = node.right
+            node.right = node.left
+            node.left = None
+        
+        tail = right_tail or left_tail or node
+        return tail
+    
+    dfs(root)
