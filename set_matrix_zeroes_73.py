94d3e6e6a2c3ba4702bc2309c8bf81940cba6bfd
Chris Schnabel <chrisjschnabel@gmail.com>
2024-02-15 22:30:44 -0500

    Optimal solution to 73 - Set Matrix Zeroes (using O(1) extra space)

--- set_matrix_zeroes_73.py
+++ set_matrix_zeroes_73.py
@@ -18,3 +18,31 @@ def set_zeroes_with_space(matrix: List[List[int]]) -> None:
             if i in rows or j in cols:
                 matrix[i][j] = 0
 
+
+# Using O(1) extra space
+def set_zeroes_with_no_extra_space(matrix: List[List[int]]) -> None:
+    m = len(matrix)
+    n = len(matrix[0])
+
+    should_zero_first_row = any(matrix[0][j] == 0 for j in range(n))
+    should_zero_first_col = any(matrix[i][0] == 0 for i in range(m))
+
+    for i in range(1, m):
+        for j in range(1, n):
+            if matrix[i][j] == 0:
+                matrix[i][0] = 0
+                matrix[0][j] = 0
+
+    for i in range(1, m):
+        for j in range(1, n):
+            if matrix[i][0] == 0 or matrix[0][j] == 0:
+                matrix[i][j] = 0
+
+    if should_zero_first_row:
+        for j in range(n):
+            matrix[0][j] = 0
+
+    if should_zero_first_col:
+        for i in range(m):
+            matrix[i][0] = 0
+
