bf1f4df0822487048b76353e0bee5d73c0a5f958
Chris Schnabel <chrisjschnabel@gmail.com>
2024-03-01 19:35:16 -0500

    BFS solution to 207 - Course Schedule

--- course_schedule_207.py
+++ course_schedule_207.py
@@ -1,4 +1,5 @@
 from typing import List
+from collections import deque
 
 def can_finish(numCourses: int, prereqs: List[List[int]]) -> bool:
     prereq_map = build_prereq_map(numCourses, prereqs)
@@ -37,3 +38,32 @@ def build_prereq_map(numCourses, prereqs):
         m[course].append(p[1])
 
     return m
+
+
+def can_finish_bfs(numCourses: int, prereqs: List[List[int]]) -> bool:
+    if not prereqs:
+        return True
+
+    prereq_map = defaultdict(set)
+    indegree_map = defaultdict(set)
+    for prereq, course in prereqs:
+        prereq_map[prereq].add(course)
+        indegree_map[course].add(prereq)
+
+    q = deque()
+    for i in range(numCourses):
+        if not indegree_map[i]:
+            q.append(i)
+
+    num_visited = 0
+
+    while q:
+        node = q.popleft()
+        num_visited += 1
+        for nei in prereq_map[node]:
+            if node in indegree_map[nei]:
+                indegree_map[nei].remove(node)
+            if not indegree_map[nei]:
+                q.append(nei)
+    
+    return num_visited == numCourses
