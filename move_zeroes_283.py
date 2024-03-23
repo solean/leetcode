0382f2ec24baffdbd4cce4e03ad1b9fe6d04f011
Chris Schnabel <chrisjschnabel@gmail.com>
2024-03-22 19:24:29 -0700

    Another solution for 283 - Move Zeroes

--- move_zeroes_283.py
+++ move_zeroes_283.py
@@ -1,30 +1,7 @@
-def moveZeroes3(nums):
-    n = len(nums)
-    result = [0] * n
+from typing import List
 
-    r = 0
-    for i in range(n):
-        if nums[i] != 0:
-            result[r] = nums[i]
-            r += 1
 
-    print(result)
-
-    for j in range(n):
-        nums[j] = result[j]
-
-def moveZeroes2(nums):
-    last_non_zero_found = 0
-
-    for i in range(len(nums)):
-        if nums[i] != 0:
-            nums[last_non_zero_found] = nums[i]
-            last_non_zero_found += 1
-
-    for i in range(last_non_zero_found, len(nums)):
-        nums[i] = 0
-
-def moveZeroes(nums):
+def moveZeroes(nums: List[int]) -> None:
     last = 0
     for i in range(len(nums)):
         if nums[i] != 0:
@@ -32,7 +9,16 @@ def moveZeroes(nums):
             last += 1
 
 
-# Expect: [1, 3, 12, 0, 0]
-nums = [0, 1, 0, 3, 12]
-moveZeroes(nums)
-print(nums)
+def moveZeroes2024Submission(nums: List[int]) -> None:
+    i, j = 0, 0
+
+    while j < len(nums):
+        if nums[j]:
+            nums[i] = nums[j]
+            i += 1
+
+        j += 1
+
+    for k in range(i, len(nums)):
+        nums[k] = 0
+
