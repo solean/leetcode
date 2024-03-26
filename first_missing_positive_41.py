5a744957fe16bedba850867b8e29e0979735d560
Chris Schnabel <chrisjschnabel@gmail.com>
2024-03-26 19:11:13 -0400

    Optimal solution to 41 - First Missing Positive

--- first_missing_positive_41.py
+++ first_missing_positive_41.py
@@ -1,5 +1,25 @@
 from typing import List
 
+def firstMissingPositiveOptimalSpace(nums: List[int]) -> int:
+    n = len(nums)
+
+    for i in range(n):
+        if nums[i] <= 0:
+            nums[i] = n + 1
+
+    for i in range(n):
+        idx = abs(nums[i]) - 1
+
+        if 0 <= idx < n and nums[idx] > 0:
+            nums[idx] = -1 * nums[idx]
+
+    for i in range(n):
+        if nums[i] > 0:
+            return i + 1
+
+    return n + 1
+
+
 def firstMissingPositive(nums: List[int]) -> int:
     minint = float('inf')
     maxint = float('-inf')
