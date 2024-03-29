7f18e4cb10da1c2121ec3cef4683c5bde611608d
Chris Schnabel <chrisjschnabel@gmail.com>
2024-03-29 18:39:55 -0400

    Removed test folder, don't feel like writing tests lol

--- test/test_container_with_most_water_11.py
+++
@@ -1,8 +0,0 @@
-from container_with_most_water_11 import max_area
-import unittest
-
-class TestMaxArea(unittest.TestCase):
-
-    def test_1(self):
-        self.assertEqual(max_area([1,8,6,2,5,4,8,3,7]), 49)
-
