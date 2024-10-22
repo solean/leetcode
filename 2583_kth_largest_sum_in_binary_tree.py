60a1293523c9a817fb81f1420ee018faf3445a8a
Chris Schnabel <chrisjschnabel@gmail.com>
2024-10-22 18:30:32 -0400

    Optimal solution to 2583 - Kth Largest Sum in Binary Tree

--- 2583_kth_largest_sum_in_binary_tree.py
+++ 2583_kth_largest_sum_in_binary_tree.py
@@ -1,5 +1,6 @@
 from misc import TreeNode
 from collections import deque
+import heapq
 
 def kthLargestLevelSum(root: TreeNode, k: int) -> int:
     q = deque()
@@ -21,4 +22,28 @@ def kthLargestLevelSum(root: TreeNode, k: int) -> int:
         levels.append(level_sum)
 
     return levels[k - 1] if k <= len(levels) else -1
+
+
+def kthLargestLevelSumOptimized(root: TreeNode, k: int) -> int:
+    q = deque()
+    q.append(root)
+    min_heap = []
+
+    while q:
+        level_sum = 0
+
+        for _ in range(len(q)):
+            node = q.popleft()
+            level_sum += node.val
+
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+
+        heapq.heappush(min_heap, level_sum)
+        if len(min_heap) > k:
+            heapq.heappop(min_heap)
+
+    return min_heap[0] if k <= len(min_heap) else -1
 
