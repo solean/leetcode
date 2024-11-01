c17bcefda02acab6e3aed40e2f42dad99e34e1b4
Chris Schnabel <chrisjschnabel@gmail.com>
2024-11-01 19:28:27 -0400

    Simpler solution to 13 - Roman to Integer

--- 13_roman_to_integer.py
+++ 13_roman_to_integer.py
@@ -1,49 +1,32 @@
 
 def romanToInt(s: str) -> int:
-    num = 0
+    values = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+        "IV": 4,
+        "IX": 9,
+        "XL": 40,
+        "XC": 90,
+        "CD": 400,
+        "CM": 900,
+    }
+
+    res = 0
+
     i = 0
-
     while i < len(s):
-        ch = s[i]
-        next_ch = s[i + 1] if i < len(s) - 1 else None
+        if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
+            res += values[s[i + 1]] - values[s[i]]
+            i += 1
+        else:
+            res += values[s[i]]
 
-        if ch == 'I':
-            if next_ch == 'V':
-                num += 4
-                i += 1
-            elif next_ch == 'X':
-                num += 9
-                i += 1
-            else:
-                num += 1
-        elif ch == 'V':
-            num += 5
-        elif ch == 'X':
-            if next_ch == 'L':
-                num += 40
-                i += 1
-            elif next_ch == 'C':
-                num += 90
-                i += 1
-            else:
-                num += 10
-        elif ch == 'L':
-            num += 50
-        elif ch == 'C':
-            if next_ch == 'D':
-                num += 400
-                i += 1
-            elif next_ch == 'M':
-                num += 900
-                i += 1
-            else:
-                num += 100
-        elif ch == 'D':
-            num += 500
-        elif ch == 'M':
-            num += 1000
-    
         i += 1
 
-    return num
+    return res
 
