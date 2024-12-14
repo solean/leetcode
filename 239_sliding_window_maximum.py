4487cc363a0d6be4e9530112b46294960ef36282
Chris Schnabel <chrisjschnabel@gmail.com>
2024-12-14 16:08:19 -0500

    Optimal solution to 239 - Sliding Window Maximum

--- 239_sliding_window_maximum.py
+++ 239_sliding_window_maximum.py
@@ -1,5 +1,35 @@
 from typing import List
+from collections import deque
 import heapq
+
+
+def maxSlidingWindowOptimal(nums: List[int], k: int) -> List[int]:
+    if len(nums) <= 1:
+        return nums
+
+    mq = deque()
+    for i in range(k):
+        while mq and nums[i] >= mq[-1][0]:
+            mq.pop()
+        mq.append((nums[i], i))
+
+    res = [mq[0][0]]
+
+    i = 1
+    j = k
+
+    while j < len(nums):
+        if mq and mq[0][1] <= j - k:
+            mq.popleft()
+
+        while mq and nums[j] >= mq[-1][0]:
+            mq.pop()
+
+        mq.append((nums[j], j))
+        res.append(mq[0][0])
+
+    return res
+
 
 def maxSlidingWindow(nums: List[int], k: int) -> List[int]:
     if len(nums) <= 1:
