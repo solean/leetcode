757ded40c7c5fdde990efa2f51f7df613b0021d5
Chris Schnabel <chrisjschnabel@gmail.com>
2025-02-10 00:37:56 -0500

    Solution to 3174 - Clear Digits

---
+++ 3174_clear_digits.py
@@ -0,0 +1,12 @@
+
+def clearDigits(s: str) -> str:
+    stack = []
+
+    for i in range(len(s)):
+        if s[i].isdigit():
+            stack.pop()
+        else:
+            stack.append(s[i])
+
+    return "".join(stack)
+
