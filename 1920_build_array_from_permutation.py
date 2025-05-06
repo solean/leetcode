bb665d26b3d306e83bd2135509be3b980211bbb8
Chris Schnabel <chrisjschnabel@gmail.com>
2025-05-05 20:41:16 -0700

    Solution to 1920 - Build Array from Permutation

---
+++ 1920_build_array_from_permutation.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def buildArray(nums: List[int]) -> List[int]:
+    n = len(nums)
+    res = [0] * n
+
+    for i in range(n)
+        res[i] = nums[nums[i]]
+
+    return res
+
