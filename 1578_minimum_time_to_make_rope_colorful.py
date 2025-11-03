da1fca566a7fce98764a104e7ab1f8114bdef02e
Chris Schnabel <chrisjschnabel@gmail.com>
2025-11-03 16:57:33 -0700

    Optimal solution to 1578 - Minimum Time to Make Rope Colorful

--- 1578_minimum_time_to_make_rope_colorful.py
+++ 1578_minimum_time_to_make_rope_colorful.py
@@ -1,6 +1,24 @@
 from typing import List
 import heapq
 
+def minCostOptimal(colors: str, neededTime: List[int]) -> int:
+    time = 0
+    curr_max = neededTime[0]
+    curr_time = neededTime[0]
+
+    for i in range(1, len(colors) + 1):
+        if i < len(colors) and colors[i] == colors[i - 1]:
+            curr_max = max(curr_max, neededTime[i])
+            curr_time += neededTime[i]
+        else:
+            time += (curr_time - curr_max)
+            if i < len(colors):
+                curr_max = neededTime[i]
+                curr_time = neededTime[i]
+
+    return time
+
+
 def minCost(colors: str, neededTime: List[int]) -> int:
     time = 0
     multiples = [neededTime[0]]
